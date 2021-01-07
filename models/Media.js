6fce4156ed2ae03e1c9ca912fe434df698b3f8a4
Quinton Fults <quinton.fults.work@gmail.com>
2021-01-07 13:35:02 -0800

    code

--- models/Media.js
+++ models/Media.js
@@ -2,7 +2,7 @@ const { model, Schema } = require('mongoose')
 
 module.exports = model('Media', new Schema({
   title: String,
-  year: String,
+  year: Number,
   imdbID: String,
   type: String,
   poster: String
